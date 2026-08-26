8367e169518e75aa58bfeedc48596aa23d731eb2
Fadi Subair <fadisubair.s@gmail.com>
2026-08-26 09:38:33 +0530

    Delete frontend directory

--- frontend/src/api/events.ts
+++
@@ -1,7 +0,0 @@
-import { apiClient } from './client';
-import { Event } from '../types';
-
-export const getEvents = async (params?: any): Promise<Event[]> => {
-  const response = await apiClient.get('/api/events', { params });
-  return response.data;
-};
--- frontend/src/api/timeline.ts
+++
@@ -1,7 +0,0 @@
-import { apiClient } from './client';
-import { TimelineEvent } from '../types';
-
-export const getIncidentTimeline = async (incidentId: string, params?: any): Promise<TimelineEvent[]> => {
-  const response = await apiClient.get(`/api/incidents/${incidentId}/timeline`, { params });
-  return response.data;
-};
